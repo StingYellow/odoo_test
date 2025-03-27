a8c90f9ea02cb851b004d04542faff3bcbf88faf
river <103347314+StingYellow@users.noreply.github.com>
2025-03-27 15:42:52 +0700

    hhh

--- om_hospital/models/appointment.py
+++ om_hospital/models/appointment.py
@@ -13,8 +13,19 @@ class Appointment(models.Model):
     ref=fields.Char(string="Reference")
 
     prescription = fields.Html(string="Prescription")
+    priority = fields.Selection([('0','Normal'),('1','Low'),('2','High'),('3','Very High'),('4','Very High1'),('5','Very High2')],string="Priority")
+    state = fields.Selection(
+        [('nhap', 'Nhap'), ('dang xac thuc', 'Dang xac thuc'), ('xac nhan', 'Xac nhan'),  ('huy', 'Huy')],
+        string="Status", default='nhap')
 
 
     @api.onchange('patient_id')
     def onchange_patient_id(self):
         self.ref = self.patient_id.ref
+
+    def action_open_facebook(self):
+        return {
+            'type': 'ir.actions.act_url',
+            'url': 'https://www.facebook.com/',
+            'target': 'new',  # Mở trong tab mới
+        }
